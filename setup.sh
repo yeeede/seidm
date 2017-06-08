79f5e5f7ab94850c72307a022d5f15150f011feb
yeeede <yeeede@gmail.com>
2017-06-08 19:21:00 +0000

    correct setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ sudo pip install virtualenv
 virtualenv ~/.env
 source ~/.env/bin/activate
 xargs sudo apt-get install -y < pkglist
-pip install requirement.txt
+pip install -r requirements.txt
 mysql-ctl start
 mysql -u root -e "CREATE DATABASE demo CHARACTER SET UTF8;"
 mysql -u root -e "CREATE USER demouser@localhost IDENTIFIED BY 'demo1234';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON demo.* to demouser@localhost;"
 mysql -u root -e "FLUSH PRIVILEGES;"
-/home/ubuntu/workspace/seidm/manage.py migrate
+~/workspace/seidm/manage.py migrate
